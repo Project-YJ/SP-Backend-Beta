d6ba55521ee1d6933738152f83fbaa58a6431fab
이준서 <ljchan71@gmail.com>
2021-09-08 22:33:24 +0900

    Start

---
+++ src/main/java/com/project/yjshop/YjShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.yjshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/yjshop/YjShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yjshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
