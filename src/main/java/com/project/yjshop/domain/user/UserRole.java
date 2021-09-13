afa7aac73d9c3f67baced2a965e2e83288913ec9
이준서 <ljchan71@gmail.com>
2021-09-13 21:27:02 +0900

    add::UserRole

---
+++ src/main/java/com/project/yjshop/domain/user/UserRole.java
@@ -0,0 +1,15 @@
+package com.project.yjshop.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+
+    USER("ROLE_USER", "유저"),
+    ADMIN("ROLE_ADMIN", "관리자");
+
+    private final String key;
+    private final String title;
+}
