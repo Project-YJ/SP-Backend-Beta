14e003824800e02fa23c6e103b816f27e41b6396
이준서 <ljchan71@gmail.com>
2021-09-13 23:15:54 +0900

    add::LoginRequest

--- src/main/java/com/project/yjshop/web/payload/request/auth/LoginRequest.java
+++ src/main/java/com/project/yjshop/web/payload/request/auth/LoginRequest.java
@@ -1,4 +1,10 @@
 package com.project.yjshop.web.payload.request.auth;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    private String email;
+    private String userid;
+    private String password;
 }
