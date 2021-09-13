014507676600c1ec90f82371101beb662c19624b
이준서 <ljchan71@gmail.com>
2021-09-13 23:29:21 +0900

    add::PrincipalDetailsService

---
+++ src/main/java/com/project/yjshop/security/auth/PrincipalDetailsService.java
@@ -0,0 +1,19 @@
+package com.project.yjshop.security.auth;
+
+import com.project.yjshop.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
