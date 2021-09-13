e7b8a89272958fc8ed9f193ffc16cc3fff274107
이준서 <ljchan71@gmail.com>
2021-09-13 23:04:29 +0900

    add::ErrorCode

---
+++ src/main/java/com/project/yjshop/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.project.yjshop.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INVALID_PASSWORD(401, "Invalid Password"),
+    INVALID_TOKEN(401, "Invalid Token"),
+
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
+    USER_NOT_FOUND(404, "User Not Found"),
+    JOIN_FAILED(400, "Join Failed");
+
+    private final int status;
+    private final String message;
+}
