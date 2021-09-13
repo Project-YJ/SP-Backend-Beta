648c3a633c6bfe98b6b2a9f085b8a05b27a12949
이준서 <ljchan71@gmail.com>
2021-09-13 23:16:42 +0900

    make::UserRepository

--- src/main/java/com/project/yjshop/domain/user/UserRepository.java
+++ src/main/java/com/project/yjshop/domain/user/UserRepository.java
@@ -2,8 +2,10 @@ package com.project.yjshop.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserid(String userid);
+    Optional<User> findByUserid(String userid);
     Boolean existsByUseridOrNickname(String userid, String nickname);
 }
