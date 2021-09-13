7d743dbc1c3941db9ef73927154c11f8ca3d7bfc
이준서 <ljchan71@gmail.com>
2021-09-13 21:35:51 +0900

    rename::GlobalExceptionHandler

--- src/main/java/com/project/yjshop/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/yjshop/error/handler/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class PreProcessingHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
     public ResponseEntity<?> validationException(CustomValidationException e) {
