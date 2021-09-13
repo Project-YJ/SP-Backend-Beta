65649f69f3d4d19ed6194c724f8beaa5203a8c4f
이준서 <ljchan71@gmail.com>
2021-09-13 21:33:57 +0900

    add::AuthController

--- src/main/java/com/project/yjshop/web/api/AuthController.java
+++ src/main/java/com/project/yjshop/web/api/AuthController.java
@@ -1,8 +1,24 @@
 package com.project.yjshop.web.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.yjshop.service.auth.AuthServiceImpl;
+import com.project.yjshop.web.payload.request.auth.JoinRequest;
+import com.project.yjshop.web.payload.response.auth.JoinResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("user")
 public class AuthController {
 
+    private final AuthServiceImpl authService;
+
+    @PostMapping("/join")
+    public JoinResponse joinUser(@RequestBody @Valid JoinRequest joinRequest,
+                                 BindingResult binding) {
+        return authService.join(joinRequest, binding);
+    }
 }
