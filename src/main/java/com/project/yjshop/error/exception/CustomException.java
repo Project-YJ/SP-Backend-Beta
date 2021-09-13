24ed239c15978957dda2b6c4a5a3b1a33e8df322
이준서 <ljchan71@gmail.com>
2021-09-13 23:04:22 +0900

    rename|refactor::CustomException

---
+++ src/main/java/com/project/yjshop/error/exception/CustomException.java
@@ -0,0 +1,25 @@
+package com.project.yjshop.error.exception;
+
+import com.project.yjshop.error.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+@Getter
+public class CustomException extends RuntimeException{
+    @Autowired
+    private final ErrorCode errorCode;
+    private Map<String, String> errorMap;
+
+    public CustomException(ErrorCode errorCode, Map<String, String> errorMap) {
+        this.errorCode = errorCode;
+        this.errorMap = errorMap;
+    }
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
