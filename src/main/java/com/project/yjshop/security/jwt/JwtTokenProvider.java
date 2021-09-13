dfac4cee005b0501d0300210c7584c9f60e0be3d
이준서 <ljchan71@gmail.com>
2021-09-13 23:29:06 +0900

    add::JwtTokenProvider

--- src/main/java/com/project/yjshop/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/project/yjshop/security/jwt/JwtTokenProvider.java
@@ -1,4 +1,12 @@
 package com.project.yjshop.security.jwt;
 
+import com.project.yjshop.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
+
+    private final UserRepository userRepository;
 }
