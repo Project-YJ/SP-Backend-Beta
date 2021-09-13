8e6c44e2033be2be083ff36f03e3bd66f0809052
이준서 <ljchan71@gmail.com>
2021-09-13 21:30:50 +0900

    add::AuthService

--- src/main/java/com/project/yjshop/service/auth/AuthService.java
+++ src/main/java/com/project/yjshop/service/auth/AuthService.java
@@ -1,4 +1,9 @@
 package com.project.yjshop.service.auth;
 
+import com.project.yjshop.web.payload.request.auth.JoinRequest;
+import com.project.yjshop.web.payload.response.auth.JoinResponse;
+import org.springframework.validation.BindingResult;
+
 public interface AuthService {
+    JoinResponse join(JoinRequest joinRequest, BindingResult binding);
 }
