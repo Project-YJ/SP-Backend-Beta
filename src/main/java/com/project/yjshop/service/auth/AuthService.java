10875ef1fd6c57844265b98249d2783ff8da423e
이준서 <ljchan71@gmail.com>
2021-09-14 09:27:22 +0900

    rename::AuthService

--- src/main/java/com/project/yjshop/service/auth/AuthService.java
+++ src/main/java/com/project/yjshop/service/auth/AuthService.java
@@ -3,10 +3,10 @@ package com.project.yjshop.service.auth;
 import com.project.yjshop.web.payload.request.auth.JoinRequest;
 import com.project.yjshop.web.payload.request.auth.LoginRequest;
 import com.project.yjshop.web.payload.response.auth.JoinResponse;
-import com.project.yjshop.web.payload.response.auth.LoginResponse;
+import com.project.yjshop.web.payload.response.auth.TokenDto;
 import org.springframework.validation.BindingResult;
 
 public interface AuthService {
     JoinResponse join(JoinRequest joinRequest, BindingResult binding);
-    LoginResponse login(LoginRequest loginRequest, BindingResult bindingResult);
+    TokenDto login(LoginRequest loginRequest, BindingResult bindingResult);
 }
