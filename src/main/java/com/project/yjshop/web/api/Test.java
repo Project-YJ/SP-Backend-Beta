bfbd4762f5863b7d18e7fa1157e4a4791c5ef420
이준서 <ljchan71@gmail.com>
2021-09-14 23:06:46 +0900

    add::Test

--- src/main/java/com/project/yjshop/web/api/Test.java
+++ src/main/java/com/project/yjshop/web/api/Test.java
@@ -1,13 +1,31 @@
 package com.project.yjshop.web.api;
 
+import com.project.yjshop.domain.token.RefreshToken;
+import com.project.yjshop.domain.token.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class Test {
+
+    private final RefreshTokenRepository tokenRepository;
 
     @GetMapping("abc")
     public String abc() {
         return "abc";
+    }
+
+    @GetMapping("test/123")
+    public String test() { return "123"; }
+
+    @PostMapping("abc")
+    public String redis() {
+        tokenRepository.save(RefreshToken.builder()
+                .token("abc")
+                .build());
+        return "success";
     }
 }
