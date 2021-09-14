55119e7663af1127bfee51a4f48596c63e23a9cd
이준서 <ljchan71@gmail.com>
2021-09-14 19:58:36 +0900

    add::JwtExceptionFilter

---
+++ src/main/java/com/project/yjshop/security/jwt/JwtExceptionFilter.java
@@ -0,0 +1,30 @@
+package com.project.yjshop.security.jwt;
+
+import com.project.yjshop.error.CMRespDto;
+import com.project.yjshop.error.exception.CustomException;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (CustomException e) {
+            JSONObject write = new JSONObject();
+            write.put("message", e.getErrorCode().getMessage());
+            write.put("error", e.getErrorMap());
+            response.setStatus(e.getErrorCode().getStatus());
+            response.getWriter().write(write.toJSONString());
+        }
+    }
+}
