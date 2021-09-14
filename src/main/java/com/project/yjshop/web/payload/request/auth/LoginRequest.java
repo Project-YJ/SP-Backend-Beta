d8e5aaa87f32ef2d2b259d03eeebb86293ce4ddd
이준서 <ljchan71@gmail.com>
2021-09-14 09:26:42 +0900

    prevent::LoginRequest

--- src/main/java/com/project/yjshop/web/payload/request/auth/LoginRequest.java
+++ src/main/java/com/project/yjshop/web/payload/request/auth/LoginRequest.java
@@ -2,9 +2,21 @@ package com.project.yjshop.web.payload.request.auth;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class LoginRequest {
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "이메일 형식에 맞게 입력해주세요")
     private String email;
+
+    @NotBlank(message = "아이디를 입력해주세요")
+    @Size(min = 2, max = 20, message = "아이디는 최소 2글자, 최대 20글자의 아이디여야 합니다.")
     private String userid;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
 }
