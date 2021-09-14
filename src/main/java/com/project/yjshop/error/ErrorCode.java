49119854e71bee7432bda470192662b2f68c8e03
이준서 <ljchan71@gmail.com>
2021-09-14 19:59:07 +0900

    refactor::ErrorCode

--- src/main/java/com/project/yjshop/error/ErrorCode.java
+++ src/main/java/com/project/yjshop/error/ErrorCode.java
@@ -6,14 +6,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    INVALID_PASSWORD(401, "Invalid Password"),
-    INVALID_TOKEN(401, "Invalid Token"),
 
-    USER_ALREADY_EXISTS(409, "User Already Exists"),
-    USER_NOT_FOUND(404, "User Not Found"),
-    USERNAME_NOT_FOUND(404, "UserName Not Found"),
-    LOGIN_FAILED(400, "Login Failed"),
-    JOIN_FAILED(400, "Join Failed");
+    INVALID_TOKEN(401, "잘못된 TOKEN 입니다."),
+    INVALID_TOKEN_SIGNATURE(401, "잘못된 JWT 서명입니다."),
+    EXPIRATION_TOKEN(401, "만료된 JWT 토큰입니다."),
+    NOT_SUPPORTED_TOKEN(401, "지원하지 않는 토큰입니다."),
+
+
+    INVALID_PASSWORD(401, "잘못된 PASSWORD 입니다."),
+    USER_ALREADY_EXISTS(409, "USER 가 이미 존재합니다"),
+    USERID_NOT_FOUND(404, "USERID 를 찾지 못했습니다"),
+    LOGIN_FAILED(400, "LOGIN 실패"),
+    JOIN_FAILED(400, "SIGN 실패");
 
     private final int status;
     private final String message;
