d61266d919db995d8678c9ecbcc488549e34e5eb
이준서 <ljchan71@gmail.com>
2021-09-14 23:05:11 +0900

    add::AuthController

--- src/main/java/com/project/yjshop/web/api/AuthController.java
+++ src/main/java/com/project/yjshop/web/api/AuthController.java
@@ -3,8 +3,8 @@ package com.project.yjshop.web.api;
 import com.project.yjshop.service.auth.AuthServiceImpl;
 import com.project.yjshop.web.payload.request.auth.JoinRequest;
 import com.project.yjshop.web.payload.request.auth.LoginRequest;
-import com.project.yjshop.web.payload.response.auth.JoinResponse;
-import com.project.yjshop.web.payload.response.auth.TokenDto;
+import com.project.yjshop.web.payload.response.auth.CustomResponse;
+import com.project.yjshop.web.payload.response.auth.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +19,19 @@ public class AuthController {
     private final AuthServiceImpl authService;
 
     @PostMapping("/join")
-    public JoinResponse joinUser(@RequestBody @Valid JoinRequest joinRequest,
-                                 BindingResult binding) {
+    public CustomResponse<?> joinUser(@RequestBody @Valid JoinRequest joinRequest,
+                                   BindingResult binding) {
         return authService.join(joinRequest, binding);
     }
 
     @PostMapping("/login")
-    public TokenDto loginUser(@RequestBody @Valid LoginRequest loginRequest,
-                              BindingResult binding) {
+    public TokenResponse loginUser(@RequestBody @Valid LoginRequest loginRequest,
+                                   BindingResult binding) {
         return authService.login(loginRequest, binding);
+    }
+
+    @PostMapping("/reissue")
+    public TokenResponse reissue(@RequestHeader(name = "YJ-refresh") String token) {
+        return authService.reissue(token);
     }
 }
