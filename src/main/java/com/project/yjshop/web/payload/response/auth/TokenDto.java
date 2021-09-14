64160b11bf40df0241990303e1254150a39a165d
이준서 <ljchan71@gmail.com>
2021-09-14 12:52:41 +0900

    fix::TokenDto

--- src/main/java/com/project/yjshop/web/payload/response/auth/TokenDto.java
+++ src/main/java/com/project/yjshop/web/payload/response/auth/TokenDto.java
@@ -2,11 +2,11 @@ package com.project.yjshop.web.payload.response.auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class TokenDto {
     private String accessToken;
     private String refreshToken;
