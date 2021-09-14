73589de5b3dca2ab5370f92763aa730b05e3ec7d
이준서 <ljchan71@gmail.com>
2021-09-14 19:58:19 +0900

    refactor::JwtTokenProvider

--- src/main/java/com/project/yjshop/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/project/yjshop/security/jwt/JwtTokenProvider.java
@@ -1,24 +1,31 @@
 package com.project.yjshop.security.jwt;
 
 import com.project.yjshop.domain.user.UserRepository;
+import com.project.yjshop.error.ErrorCode;
+import com.project.yjshop.error.exception.CustomException;
+import com.project.yjshop.security.auth.PrincipalDetailsService;
 import com.project.yjshop.web.payload.response.auth.TokenDto;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Base64;
-import java.util.Date;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
     private final UserRepository userRepository;
+    private final PrincipalDetailsService principalDetailsService;
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -29,30 +36,52 @@ public class JwtTokenProvider {
     @Value("${jwt.header}")
     private String header;
 
-    public String init() {
-        return Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
-    }
-
     public TokenDto createToken(String username) {
 
         Date now = new Date();
-
         String accessToken =Jwts.builder()
-                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setClaims(Jwts.claims().setSubject(username))
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + Duration.ofMinutes(1).toMillis()))
-                .signWith(SignatureAlgorithm.HS256, init())
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
 
         String refreshToken = Jwts.builder()
                 .setExpiration(new Date(now.getTime() + Duration.ofDays(1).toMillis()))
-                .signWith(SignatureAlgorithm.HS256, init())
+                .setClaims(Jwts.claims().setSubject(secretKey))
                 .compact();
 
         return TokenDto.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(accessToken).getBody();
+        UserDetails userDetails = principalDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public boolean validateToken(String jwt) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt).getBody().getExpiration().after(new Date());
+        } catch (SecurityException | MalformedJwtException e) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN_SIGNATURE);
+        } catch (ExpiredJwtException e) {
+            throw new CustomException(ErrorCode.EXPIRATION_TOKEN);
+        } catch (UnsupportedJwtException e) {
+            throw new CustomException(ErrorCode.NOT_SUPPORTED_TOKEN);
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader(header);
+        if (StringUtils.hasText(token) && token.startsWith(prefix)) {
+            return token.substring(prefix.length());
+        }
+       return null;
     }
 }
