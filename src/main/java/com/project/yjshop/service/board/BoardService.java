0ee20b01253b9b4b8f7b1eacdd4976bcbea9b49a
이준서 <ljchan71@gmail.com>
2021-09-17 22:59:42 +0900

    add::BoardService

---
+++ src/main/java/com/project/yjshop/service/board/BoardService.java
@@ -0,0 +1,12 @@
+package com.project.yjshop.service.board;
+
+import com.project.yjshop.web.payload.request.board.ProductRequest;
+import com.project.yjshop.web.payload.response.board.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface BoardService {
+    ProductResponse posting(ProductRequest productRequest) throws IOException;
+    ProductResponse deleting(String boardId);
+}
