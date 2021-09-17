d1f1f5edbe595274b180814396eb9a9dbce248b2
이준서 <ljchan71@gmail.com>
2021-09-17 20:38:46 +0900

    add::S3Service

---
+++ src/main/java/com/project/yjshop/service/image/S3Service.java
@@ -0,0 +1,62 @@
+package com.project.yjshop.service.image;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@NoArgsConstructor
+public class S3Service {
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+
+    public String upload(MultipartFile image) throws IOException {
+        String fileName = UUID.randomUUID() + "-" + image.getOriginalFilename();
+
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, image.getInputStream(), null)
+            .withCannedAcl(CannedAccessControlList.PublicRead));
+        return fileName;
+    }
+
+    public String delete(String imagePath) {
+        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucket, imagePath);
+        s3Client.deleteObject(deleteObjectRequest);
+        return "delete success:"+deleteObjectRequest.getKey();
+    }
+
+}
