14cb950eab89b28bfdbb6be2c78cd6eaf93cca81
이준서 <ljchan71@gmail.com>
2021-09-19 02:28:04 +0900

    add::UserService

--- src/main/java/com/project/yjshop/service/user/UserService.java
+++ src/main/java/com/project/yjshop/service/user/UserService.java
@@ -1,2 +1,9 @@
-package com.project.yjshop.service.user;public class UserService {
+package com.project.yjshop.service.user;
+
+import com.project.yjshop.security.auth.PrincipalDetails;
+import com.project.yjshop.web.payload.request.user.PurchaseRequest;
+import com.project.yjshop.web.payload.response.user.PurchaseResponse;
+
+public interface UserService {
+    PurchaseResponse purchase(PurchaseRequest purchaseRequest, PrincipalDetails principalDetails);
 }
